6d76eb542387b2f44bf40250ddf0fa224698fec9
DOPEKID69420 <karanlath2004@gmail.com>
2025-03-01 22:47:32 +0530

    bvbvb

--- DrAi/urls.py
+++ DrAi/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import get_diseases,voice_input
+from .views import get_diseases
 from . import views
 from .views import hello_world
 
@@ -7,5 +7,5 @@ urlpatterns = [
     path('get_diseases/', get_diseases, name='get_diseases'),
     path('', views.index, name='index'),
     path('api/hello/', hello_world, name='hello_world'),
-    path("voice-input/", voice_input, name="voice_input"),
+    
 ]
